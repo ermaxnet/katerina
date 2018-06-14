ffb9700e169c8221cd6e3b731e155a4543a55dc1
Max Eremin <eremin_my@st.by>
2018-06-14 17:09:13 +0300

    get data from wikipedia by link

--- wiki-api/index.js
+++ wiki-api/index.js
@@ -31,12 +31,22 @@ class Wiki {
                     "User-Agent": this.userAgent
                 }
             }
-        );
+        ).then(response => {
+            if(response) {
+                const page = this._parse(response);
+                if(page) {
+                    page["lang"] = lang;
+                    return page;
+                }
+            }
+            return {};
+        });
     }
-    async _getId(key1, lang) {
+    async _getId(key1, lang, returnLang = false) {
         return this._send({ titles: key1 }, lang)
-            .then(response => this._parse(response))
-            .then(({ pageid }) => pageid);
+            .then(({ pageid, lang }) => 
+                returnLang ? { pageid, lang } : pageid
+            );
     }
     async getSight(keys) {
         const idsTasks = Array.from(keys.keys()).map(async lang => {
@@ -52,12 +62,11 @@ class Wiki {
             })
             .then(keys => {
                 const namesTasks = Array.from(keys.keys()).map(async lang => {
-                    const response = await this._send({ 
+                    const { displaytitle } = await this._send({ 
                         pageids: keys.get(lang).key2,
                         prop: "info",
                         inprop: "displaytitle"
                     }, lang);
-                    const { displaytitle } = this._parse(response);
                     return [ lang, displaytitle ];
                 });
                 return Promise.all(namesTasks);
@@ -74,7 +83,7 @@ class Wiki {
                 );
             })
             .then(([ response, names ]) => {
-                let { coordinates: coords } = this._parse(response);
+                let { coordinates: coords } = response;
                 if(coords) {
                     coords = {
                         lat: coords[0].lat,
@@ -92,15 +101,6 @@ class Wiki {
         if(typeof key === "string") {
             key = await this._getId(key, lang);
         }
-        const response = await this._send({
-            pageids: key,
-            prop: "extracts|pageimages|info|coordinates",
-            exintro: true,
-            explaintext: true,
-            pithumbsize: 500,
-            piprop: "thumbnail",
-            inprop: "url"
-        }, lang);
         const {
             extract: text,
             fullurl: wikiLink,
@@ -108,11 +108,61 @@ class Wiki {
             thumbnail: {
                 source: thumbnail
             }
-        } = this._parse(response);
+        } = await this._send({
+            pageids: key,
+            prop: "extracts|pageimages|info",
+            exintro: true,
+            explaintext: true,
+            pithumbsize: 500,
+            piprop: "thumbnail",
+            inprop: "url"
+        }, lang);
         return {
             text, wikiLink, updatedAt, thumbnail
         };
     }
+    async getNewSightDetail(name, lang) {
+        const {
+            langlinks: links,
+            coordinates: [ { lat, lon } ]
+        } = await this._send({
+            titles: name,
+            prop: "coordinates|langlinks",
+            lllimit: 500,
+            llprop: "url"
+        }, lang);
+        if(!links || !links.length || !(lat && lon)) {
+            return null;
+        }
+        const langs = links.filter(link => {
+            return [ "ru", "be", "en" ].includes(link.lang);
+        });
+        let names = [], tasks = [];
+        const keys = new Map(langs.map(link => {
+            names.push([ link.lang, link["*"] ]);
+            tasks.push(this._getId(link["*"], link.lang, true));
+            return [ link.lang, { key1: link["*"] } ];
+        }));
+        names = new Map(names);
+        return Promise.all(tasks)
+            .then(pages => {
+                pages.forEach(page => {
+                    if(keys.has(page.lang)) {
+                        keys.get(page.lang).key2 = page.pageid;
+                    }
+                });
+                return {
+                    keys, names,
+                    coords: { lat, lon }
+                };
+            });
+    }
 };
+
+const wiki = new Wiki();
+wiki.getNewSightDetail("Zamek_w_Nieświeżu", "pl")
+    .then(tasks => {
+        console.log(tasks);
+    });
 
 module.exports = Wiki;
