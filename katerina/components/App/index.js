f10a24b29086243abfa8f36703cdb2514f482b00
Max Eremin <eremin_my@st.by>
2018-06-28 16:39:47 +0300

    Fix #1

--- katerina/components/App/index.js
+++ katerina/components/App/index.js
@@ -1,12 +1,11 @@
 import React, { Component } from "react";
-import YandexMap from "../YandexMap";
 
 class App extends Component {
     render() {
         return (
             <div className="App">
                 <div className="App__map">
-                    <YandexMap />
+                    Hello, Max.
                 </div>
             </div>
         );
